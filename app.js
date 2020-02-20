f8e344f33c5cd0caa31429c56ef080e8c4337dd2
Axel Duval <axel.duval74@gmail.com>
2020-02-20 19:14:51 +0100

    testing

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const express = require('express')
-const app = express()
+const router = express.Router()
 const PORT = process.env.PORT || 5000
+
+//Require controller modules
+
+
+/// INTERNAUTE ROUTES ///
+/// CAMPEUR ROUTES ///
+/// PERSONNEL ROUTES ///
+/// MANAGER ROUTES ///
 
 
 const server = app.listen(PORT, () => {
     console.log('Server is running on port 3000...');
 });
+
+router.get('/',(req,res)=>{
+    res.send("J'ai réussi à héberger mon site avec Hiroku !")
+})
+
+// Errors => page not found 404
+app.use((req, res, next) =>  {
+    var err = new Error('Page not found');
+    err.status = 404
+    next(err)
+})
+
+// Handling errors (send them to the client)
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send(err.message)
+})
+
+module.exports = router
