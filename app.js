1f9d1f26eb746ad7ef2247d4800e073464f90b5b
Axel Duval <axel.duval74@gmail.com>
2020-02-20 18:49:45 +0100

    first test web server

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
 
 
-const server = app.listen(3000, () => {
+const server = app.listen(PORT, () => {
     console.log('Server is running on port 3000...');
 });
