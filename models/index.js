4772139fc186939e09a96ab3aa04b4a35c5f7371
Axel Duval <axel.duval74@gmail.com>
2020-02-25 18:34:14 +0100

    Error pushing on heroku

---
+++ models/index.js
@@ -0,0 +1,116 @@
+const db = require('./pool')
+const jwt = require('jsonwebtoken')
+const constants = require("../constants")
+
+const index = {
+
+    findCampeurById : async (id)=>{
+        return new Promise((resolve, reject) => {
+            db.query("SELECT * FROM campeurs WHERE id=?", [id], (err,rows)=>{
+                if (err) {
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+        })
+    },
+
+    verifyUser : async (token)=>{
+        return new Promise((resolve, reject) => {
+            jwt.verify(token,constants.SECRET_KEY,(err,auth)=>{
+                if(err){
+                    reject(err)
+                }
+                else{
+                    resolve(auth)
+                }
+            })
+        })
+    },
+
+    findCampeurByEmail : async (email)=>{
+        return new Promise((resolve, reject) => {
+            db.query("SELECT * FROM campeurs WHERE mail=?", [email], (err,rows)=>{
+                if (err) {
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+        })
+    },
+
+    findPersonnelByEmail : async (email)=>{
+        return new Promise((resolve, reject) => {
+            db.query("SELECT * FROM personnel WHERE identifiant=?", [email], (err,rows)=>{
+                if (err) {
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+        })
+    },
+
+    createToken : (params)=>{
+        return jwt.sign(params,constants.SECRET_KEY,{expiresIn: '5h'})
+    },
+
+    createCampeur : async (nom,prenom,mail,telephone,password)=>{
+        return new Promise((resolve, reject) => {
+            db.query("INSERT INTO campeurs(nom, prenom, mail, telephone, password) VALUES (?,?,?,?,?)", [nom,prenom,mail,telephone,password], (err,rows)=>{
+                if (err) {
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+        })
+    },
+
+    findReservPossibleTent : async (arr,dep)=>{
+        return new Promise((resolve, reject) => {
+            db.query("SELECT emplacements.id, chalets.nom , chalets.prestige FROM emplacements LEFT JOIN chalets ON chalets.id = emplacements.idChalet WHERE emplacements.idChalet IS null AND emplacements.id NOT IN( SELECT reservations.idEmplacement FROM reservations WHERE ? <= reservations.dateFin AND ? >= reservations.dateDebut)", [arr,dep], (err,rows)=>{
+                if (err) {
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+        })
+    },
+
+    findReservPossibleChalet : async (val,arr,dep)=>{
+        return new Promise((resolve, reject) => {
+            db.query("SELECT emplacements.id, chalets.nom , chalets.prestige FROM emplacements LEFT JOIN chalets ON chalets.id = emplacements.idChalet WHERE chalets.prestige=? AND emplacements.id NOT IN( SELECT reservations.idEmplacement FROM reservations WHERE ? <= reservations.dateFin AND ? >= reservations.dateDebut)", [val,arr,dep], (err,rows)=>{
+                if (err) {
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+        })
+    },
+
+    findReservPossible : async (arr,dep)=>{
+        return new Promise((resolve, reject) => {
+            db.query("SELECT emplacements.id, chalets.nom , chalets.prestige FROM emplacements LEFT JOIN chalets ON chalets.id = emplacements.idChalet WHERE emplacements.id NOT IN( SELECT reservations.idEmplacement FROM reservations WHERE ? <= reservations.dateFin AND ? >= reservations.dateDebut)", [arr,dep], (err,rows)=>{
+                if (err) {
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+        })
+    }
+}
+
+module.exports = index;
